484e0a9f632cf03463742e7edbd184ef1ff039ab
孙思杰 <sunsijie@buaa.edu.cn>
2015-11-14 08:01:33 +0800

    wildcard

--- curr/main.cpp
+++ curr/main.cpp
@@ -1,4 +1,35 @@
-int main()
-{
-	return 1;
-}
+#include<iostream>
+#include<map>
+using namespace std;
+class Solution {
+	string t;
+	string p;
+	int tend;
+	int pend;
+	map<int,map<int,int>> cache;
+	bool test(int tidx, int pidx)
+	{
+		auto& c = cache[tidx][pidx];
+		if(c != 0)return c == 1?true:false;
+		if(tidx == tend && pidx == pend)return true;
+		if(tidx == tend || pidx == pend)return false;
+
+		switch(p[pidx])
+		{
+			case '*':
+				break;
+			case '*':
+				break;
+			default:
+				break;
+		}
+	}
+public:
+    bool isMatch(string t, string p) {
+		this->t = t;
+		this->p = p;
+		tend = t.size();
+		pend = p.size();
+		return test(0,0);
+    }
+};
