fd8f4394c1a7a418508fe99110c259881fc0da5c
孙思杰 <sunsijie@buaa.edu.cn>
2015-12-02 01:36:30 +0800

    d

---
+++ Accepted/Easy/DeleteNodeInSingleList.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+#include<cstdlib>
+
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode(int x) : val(x), next(NULL) {}
+ };
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        if(!node->next->next)
+        {
+            delete node->next;
+            node->next = nullptr;
+        }
+        else
+        {
+            node->val = node->next->val;
+            deleteNode(node->next);
+        }
+    }
+};
