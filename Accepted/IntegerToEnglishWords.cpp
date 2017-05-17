cdca884350bc1b4d67d0e0bbbee2e9358b7ced36
sunsijie <sunsijie@buaa.edu.cn>
2017-05-17 19:06:50 +0800

    Integer to English Words

---
+++ Accepted/IntegerToEnglishWords.cpp
@@ -0,0 +1,109 @@
+#include<iostream>
+#include<sstream>
+#include<vector>
+#include<map>
+#include<climits>
+#include<algorithm>
+#include<cstdlib>
+#include<cstring>
+#include <cmath>
+using namespace std;
+
+class Solution {
+	map<int, string> spec = {
+		{1,"One"},
+		{2,"Two"},
+		{3,"Three"},
+		{4,"Four"},
+		{5,"Five"},
+		{6,"Six"},
+		{7, "Seven"},
+		{8, "Eight"},
+		{9, "Nine"},
+		{10, "Ten"},
+		{11, "Eleven"},
+		{12, "Twelve"},
+		{13, "Thirteen"},
+		{14, "Fourteen"},
+		{15, "Fifteen"},
+		{16, "Sixteen"},
+		{17, "Seventeen"},
+		{18, "Eighteen"},
+		{19, "Nineteen"},
+	};
+	map<int, string> ten = {
+		{2, "Twenty"},
+		{3, "Thirty"},
+		{4, "Forty"},
+		{5, "Fifty"},
+		{6, "Sixty"},
+		{7, "Seventy"},
+		{8, "Eighty"},
+		{9, "Ninety"},
+	};
+	map<int, string> thousand = {
+	    {1, "Thousand"},
+	    {2, "Million"},
+	    {3, "Billion"},
+	};
+
+    vector<string> getLessThanThousand(int ge, int shi, int bai)
+    {
+        vector<string> res;
+        if (bai > 0)
+        {
+            res.push_back(spec[bai]);
+            res.push_back("Hundred");
+        }
+        if (shi == 0 && ge == 0)
+        {
+            return res;
+        }
+        if (shi <= 1)
+        {
+            res.push_back(spec[shi * 10 + ge]);
+        }
+        else
+        {
+            res.push_back(ten[shi]);
+            if (ge > 0) res.push_back(spec[ge]);
+        }
+        return res;
+    }
+public:
+    string numberToWords(int num) {
+        vector<string> res;
+        int i = 0;
+        while(num)
+        {
+            int ge = num % 10; num /= 10;
+            int shi = num % 10; num /= 10;
+            int bai = num % 10; num /= 10;
+            auto ret = getLessThanThousand(ge, shi, bai);
+            if (i > 0 && !ret.empty())
+            {
+                res.insert(res.begin(), thousand[i]);
+            }
+            res.insert(res.begin(), ret.begin(), ret.end());
+            i++;
+        }
+        if (res.empty())
+        {
+            return "Zero";
+        }
+        stringstream ss;
+        ss << res[0];
+        for (int j = 1; j < res.size(); j++)
+        {
+            ss << " " << res[j];
+        }
+        return ss.str();
+    }
+};
+
+int main()
+{
+	Solution s;
+	cout << s.numberToWords(20) << endl;
+	return 0;
+}
